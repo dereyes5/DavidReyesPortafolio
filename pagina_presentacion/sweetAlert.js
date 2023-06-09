67eb60ee8270f6518665e7120f4fa1da161de6d8
dereyes5 <107556653+dereyes5@users.noreply.github.com>
2023-06-08 20:45:38 -0500

    Delete pagina_presentacion directory

--- pagina_presentacion/sweetAlert.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("#enviar").addEventListener("click", function() {
-    Swal.fire({
-        position: 'top-end',
-        icon: 'success',
-        title: 'Your work has been saved',
-        showConfirmButton: false,
-        timer: 1500
-    });
-});
